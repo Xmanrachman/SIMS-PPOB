be3d3936e869d8c79a7cd00dfd1767af0b0174e0
Traxman <taufik.rachman.hakim@gmail.com>
2024-12-09 21:44:28 +0700

    add all file

---
+++ SIMS-PPOB/src/main/java/com/nutech/test/sims/ppob/response/ResponseBanner.java
@@ -0,0 +1,31 @@
+package com.nutech.test.sims.ppob.response;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseBanner {
+
+	
+	private HttpStatus status;
+	
+	private String message;
+	
+	private List<Object> data;
+
+	
+	
+	
+}
