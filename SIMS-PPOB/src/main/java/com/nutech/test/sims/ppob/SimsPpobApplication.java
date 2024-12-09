781a5c1b763b499b0b43c68acd597a00750c21b9
xman <106719630+Xmanrachman@users.noreply.github.com>
2024-12-09 13:04:41 +0700

    Add project SIMS_PPOB

---
+++ SIMS-PPOB/src/main/java/com/nutech/test/sims/ppob/SimsPpobApplication.java
@@ -0,0 +1,13 @@
+package com.nutech.test.sims.ppob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsPpobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsPpobApplication.class, args);
+	}
+
+}
