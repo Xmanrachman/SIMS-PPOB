64603e047d1bbb5cca4e7d4516fe9d8a7aa77a60
Traxman <taufik.rachman.hakim@gmail.com>
2024-12-10 13:04:00 +0700

    change jwtauthenticationfilter

--- SIMS-PPOB/src/main/java/com/nutech/test/sims/ppob/authapi/config/JwtAuthenticationFilter.java
+++ SIMS-PPOB/src/main/java/com/nutech/test/sims/ppob/authapi/config/JwtAuthenticationFilter.java
@@ -29,6 +29,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
 	@Autowired
 	private final HandlerExceptionResolver handlerExceptionResolver;
+	@Autowired
     private final JwtServiceInterface jwtService;
     @Autowired
     private final UserServiceInterface userService;
